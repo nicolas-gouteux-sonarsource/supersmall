bcbfbbcddfd7f69046500c1222869396d3dc79b0
Nicolas Gouteux <nicolas.gouteux@sonarsource.com>
2023-03-14 16:43:38 +0100

    dummy

---
+++ src/main/java/org/example/Main2.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main2 {
+  public static void main(String[] args) {
+    int unusedVariable1113 = 0;
+    int unusedVariable17 = 0;
+
+    System.out.println("Hello world!");
+  }
+}
