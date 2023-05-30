39ebbb8ecb747ad5d0b383e117cde0c6bc9dad67
Nicolas Gouteux <nicolas.gouteux@sonarsource.com>
2023-05-30 09:25:09 +0200

    changed Other2 IIII

--- src/main/java/org/example/p1bis/Other223.java
+++ src/main/java/org/example/p1bis/Other223.java
@@ -1,6 +1,6 @@
 package org.example.p1bis;
 
-public class Other2 {
+public class Other223 {
   public static void main(String[] args) {
     int unusedVariable1113dcasHAZE2cdacdacXXXdassxdasi9 = 0;
   }
--- src/main/java/org/example/p1bis/Other3.java
+++ src/main/java/org/example/p1bis/Other3.java
@@ -2,6 +2,6 @@ package org.example.p1bis;
 
 public class Other3 {
   public static void main(String[] args) {
-    int unusedVariable14 = 12;
+    int unusedVariable15 = 19;
   }
 }
