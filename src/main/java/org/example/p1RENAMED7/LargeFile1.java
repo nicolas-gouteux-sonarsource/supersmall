806df1b395c47ccd72af55525d6e04a300f7d976
Nicolas Gouteux <nicolas.gouteux@sonarsource.com>
2023-10-27 12:33:06 +0200

    RENAMED

--- src/main/java/org/example/p1RENAMED7/LargeFile1.java
+++ src/main/java/org/example/p1RENAMED7/LargeFile1.java
@@ -1,4 +1,4 @@
-package org.example.p1RENAMED4;
+package org.example.p1RENAMED7;
 
 /** 
 
--- src/main/java/org/example/p1RENAMED7/file1.java
+++ src/main/java/org/example/p1RENAMED7/file1.java
@@ -1,4 +1,4 @@
-package org.example.p1RENAMED4;
+package org.example.p1RENAMED7;
 
 public class file1 {
   public static void main(String[] args) {
